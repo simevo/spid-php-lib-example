b7cf5f6774f0fbd3c2e78a29075fb3e5e03aa477
Paolo Greppi <paolo.greppi@libpf.com>
2018-10-13 08:02:50 +0200

    update dependencies

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $settings = [
     'sp_assertionconsumerservice' => [
         $base . '/acs'
     ],
-    'sp_singlelogoutservice' => $base . '/slo',
+    'sp_singlelogoutservice' => [[$base . '/slo', '']],
     'sp_org_name' => 'test',
     'sp_org_display_name' => 'Test',
     'idp_metadata_folder' => './sp_conf/',
